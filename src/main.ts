24c2590849f5cfc8308eaf123758be3c6316697c
Adrien Houle <adrienhoule01@gmail.com>
2026-06-06 17:57:54 +0200

    simple plan avec vague en sinus en utilisant le positionNode et le normalNode

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,59 @@
-import { hash, uniform, vec3, instanceIndex, float } from 'three/tsl';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { vec3, time, positionLocal, Fn, cross, normalize } from 'three/tsl';
 import * as THREE from 'three/webgpu';
 
+import type { Node } from 'three/webgpu';
+
+
+type PosParams = { position: Node<'vec3'> };
+type FloatParams = { value: Node<'float'> };
+
+const oscSine = Fn( ( {value}:FloatParams ) => {
+
+	return value.add( 0.75 ).mul( Math.PI * 2 ).sin().mul( 0.5 ).add( 0.5 );
+
+} );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const material = new THREE.MeshBasicNodeMaterial();
+const material = new THREE.MeshPhysicalNodeMaterial();
 
-const seed = uniform(float(0));
+export const WaveDisplace = Fn(({ position }: PosParams) => {
+  const tempPos = vec3(position);
 
-const r = hash(instanceIndex.add(seed));
-const g = hash(instanceIndex.add(seed).add(1));
-const b = hash(instanceIndex.add(seed).add(2));
+  const dist = position.x.mul(position.x)
+  .add(position.y.mul(position.y))
+  .sqrt();
+  const speed = 1;
+  const wave = oscSine({ value: dist.add(time.mul(speed)) });
+ const amplitude = 0.5; 
+  tempPos.z.assign(wave.mul(amplitude));
+  return tempPos;
+});
 
-material.colorNode = vec3(r, g, b);
+material.positionNode = WaveDisplace( { position: positionLocal } );
+
+export const WaveNormal = Fn(({ position }: PosParams) => {
+  
+  const eps = 0.01;
+  // on décale pour calculer par rapport a la pente 
+  const P0 = WaveDisplace({ position });
+  const Px = WaveDisplace({ position: position.add(vec3(eps, 0, 0)) });
+  const Py = WaveDisplace({ position: position.add(vec3(0, eps, 0)) });
+
+  const T1 = Px.sub(P0); 
+  const T2 = Py.sub(P0); 
+
+  return normalize(cross(T1, T2));
+});
+
+material.normalNode = WaveNormal( { position: positionLocal } );
+
+material.colorNode = vec3(1, 0.5, 0.2);
 
 const renderer = new THREE.WebGPURenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener('resize', () => {
@@ -26,28 +62,28 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-const count = 10;
-const radius = 3;
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const cube = new THREE.InstancedMesh(geometry, material, count);
+const light = new THREE.DirectionalLight(0xffffff, 2);
+light.position.set(1, 1, 1);
+scene.add(light);
 
-const dummy = new THREE.Object3D();
-for (let i = 0; i < count; i++) {
-  const angle = (i / count) * Math.PI * 2;
-  dummy.position.set(Math.cos(angle) * radius, Math.sin(angle) * radius, 0);
-  dummy.updateMatrix();
-  cube.setMatrixAt(i, dummy.matrix);
-}
-cube.instanceMatrix.needsUpdate = true;
-scene.add(cube);
+const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambient);
+
+const geometry = new THREE.PlaneGeometry(32,32, 256, 256);
+const plane = new THREE.Mesh(geometry, material);
+
+scene.add(plane);
 scene.background = new THREE.Color(0, 0, 0);
 
 camera.position.z = 5;
 
+ const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
 await renderer.init();
 
-function animate(time: number) {
-  seed.value = time / 100;
-
+renderer.setAnimationLoop(animate);
+function animate(/* time: number */) {
+  controls.update();
   renderer.render(scene, camera);
 }
